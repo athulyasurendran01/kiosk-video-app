07c9a665aeb38e919772fab4fca68a6e16bc1594
athulyasurendran <athulyasurendran01@gmail.com>
2017-10-27 11:22:31 -0700

    #

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1525;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
@@ -14,36 +14,9 @@ const server = express()
 const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
-	
-  ws.on('close', () => console.log('Client disconnected'));
-  
   console.log('Client connected');
-  
-  console.log("A new WebSocket client was connected.");
- 
-  client.on('message', function (message) {
-    
-  wss.broadcast(message, client);
-  
-  
-   });
-  
+  ws.on('close', () => console.log('Client disconnected'));
 });
-
-
-wss.broadcast = function (data, exclude) {
-  var i = 0, n = this.clients ? this.clients.length : 0, client = null;
-  if (n < 1) return;
-  console.log("Broadcasting message to all " + n + " WebSocket clients.");
-  for (; i < n; i++) {
-    client = this.clients[i];
-    // don't send the message to the sender...
-    if (client === exclude) continue;
-    if (client.readyState === client.OPEN) client.send(data);
-    else console.error('Error: the client state is ' + client.readyState);
-  }
-};
-
 
 setInterval(() => {
   wss.clients.forEach((client) => {
