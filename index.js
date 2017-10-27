4f43867923d153f604e96fff802ffea44fdd097b
athulyasurendran <athulyasurendran01@gmail.com>
2017-10-27 10:02:37 -0700

    #

--- index.js
+++ index.js
@@ -11,48 +11,15 @@ const pkey = fs.readFileSync('./public/ssl/key.pem'),
 var wss = null, sslSrv = null;
  
  
-
+ 
+wss = new WebSocketServer({server: sslSrv});  
 
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.use(function(req, res, next) {
-  if(req.headers['x-forwarded-proto']==='http') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  next();
-});
-
-// create the WebSocket server
-wss = new WebSocketServer({server: sslSrv});  
-
-/** successful connection 
-wss.on('connection', function (client) {
-  console.log("A new WebSocket client was connected.");
- 
-  client.on('message', function (message) {
-    
-    wss.broadcast(message, client);
-  });
-});
-
-wss.broadcast = function (data, exclude) {
-  var i = 0, n = this.clients ? this.clients.length : 0, client = null;
-  if (n < 1) return;
-  console.log("Broadcasting message to all " + n + " WebSocket clients.");
-  for (; i < n; i++) {
-    client = this.clients[i];
-    // don't send the message to the sender...
-    if (client === exclude) continue;
-    if (client.readyState === client.OPEN) client.send(data);
-    else console.error('Error: the client state is ' + client.readyState);
-  }
-};*/
-
-
 app.get('/', function(request, response) {
-  response.send('Hello !')
+  response.send('Hello hai !')
 })
 
 app.listen(app.get('port'), function() {
