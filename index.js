76f8398d85eff8f889b6b08ca57e3e741ff4350a
athulyasurendran <athulyasurendran01@gmail.com>
2017-10-27 09:40:21 -0700

    #

--- index.js
+++ index.js
@@ -1,67 +1,13 @@
-const WebSocketServer = require('ws').Server,
-  express = require('express'),
-  https = require('https'),
-  app = express(),
-  fs = require('fs');
-/*
-const pkey = fs.readFileSync('./ssl/key.pem'),
-  pcert = fs.readFileSync('./ssl/cert.pem'),
-  options = {key: pkey, cert: pcert, passphrase: '123456789'};
-var wss = null, sslSrv = null;
- */
-// use express static to deliver resources HTML, CSS, JS, etc)
-// from the public folder 
-app.use(express.static('public'));
+var express = require('express')
+var app = express()
 
-/*app.use(function(req, res, next) {
-	console.log("Redirect");
-  if(req.headers['x-forwarded-proto']==='http') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  next();
-});*/
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-// start server (listen on port 443 - SSL)
+app.get('/', function(request, response) {
+  response.send('Hello !')
+})
 
-sslSrv = https.createServer(options, app).listen(process.env.PORT || 3000);
-console.log("The HTTPS server is up and running");
-
-// create the WebSocket server
-/**wss = new WebSocketServer({server: sslSrv});  
-console.log("WebSocket Secure server is up and running.");
- successful connection 
-wss.on('connection', function (client) {
-  console.log("A new WebSocket client was connected.");
- 
-  client.on('message', function (message) {
-    
-    wss.broadcast(message, client);
-  });
-});
-// broadcasting the message to all WebSocket clients.
-wss.broadcast = function (data, exclude) {
-  var i = 0, n = this.clients ? this.clients.length : 0, client = null;
-  if (n < 1) return;
-  console.log("Broadcasting message to all " + n + " WebSocket clients.");
-  for (; i < n; i++) {
-    client = this.clients[i];
-    // don't send the message to the sender...
-    if (client === exclude) continue;
-    if (client.readyState === client.OPEN) client.send(data);
-    else console.error('Error: the client state is ' + client.readyState);
-  }
-};
-
-{
-  mandatory: {
-    width: { min: 640 }
-    height: { min: 480 }
-  }
-  optional: [
-    { width: 650 },
-    { width: { min: 650 }},
-    { frameRate: 60 },
-    { width: { max: 800 }},
-    { facingMode: "user" }
-  ]
-}*/
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
